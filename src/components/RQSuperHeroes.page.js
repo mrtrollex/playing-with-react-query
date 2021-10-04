9aa8bba25058294910a7cb2806e49ab2f2464e72
mrtrollex <619ricardo619@gmail.com>
2021-10-05 00:46:52 +0200

    fetching via react-query

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,4 +1,24 @@
+import { useQuery } from 'react-query';
+import axios from 'axios';
+import React from 'react';
+
+const fetchSuperHeroes = () => {
+  return axios.get('http://localhost:4000/superheroes');
+};
+
 export const RQSuperHeroesPage = () => {
-    return <div>RQ Super Heroes Page</div>;
-  };
-  
+  const { isLoading, data } = useQuery('super-heroes', fetchSuperHeroes);
+
+  if (isLoading) {
+    return <h2>Loading..</h2>;
+  }
+
+  return (
+    <React.Fragment>
+      <h2>RQ Super Heroes Page</h2>
+      {data?.data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
+    </React.Fragment>
+  );
+};
